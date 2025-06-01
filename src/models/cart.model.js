6727da22bd6c0bf5183476fdcb149f39256cc567
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-06-01 18:44:54 -0300

    se modifica el archivo cart.model.js

---
+++ src/models/cart.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        quantity: { type: Number, required: true, default: 1 }
+      }
+    ]
+  },
+  {
+    timestamps: true,   
+    versionKey: false  
+  }
+);
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
