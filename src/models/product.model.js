ff6f7a60eca8e7f2b4fe4a4d372faf08d0d97146
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-06-01 18:42:13 -0300

    se modifica el archivo product.model.js

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  thumbnails: { type: [String], default: [] }, // arreglo de urls
-  code: { type: String, required: true, unique: true },
-  stock: { type: Number, required: true },
-  status: { type: Boolean, default: true },
-  category: { type: String, required: true }
-});
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    thumbnails: { type: [String], default: [] }, // arreglo de URLs
+    code: { type: String, required: true, unique: true },
+    stock: { type: Number, required: true },
+    status: { type: Boolean, default: true },
+    category: { type: String, required: true }
+  },
+  {
+    timestamps: true, // agrega createdAt y updatedAt automáticamente
+    versionKey: false // elimina el campo __v que Mongoose agrega por defecto
+  }
+);
 
 const Product = mongoose.model('Product', productSchema);
 
