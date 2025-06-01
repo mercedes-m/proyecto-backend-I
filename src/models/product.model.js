315f83f9908ff420e75758ca038d5518f97adff9
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-06-01 19:39:05 -0300

    se modifica product.model.js para incluir la paginación

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,22 +1,18 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
-const productSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    thumbnails: { type: [String], default: [] }, // arreglo de URLs
-    code: { type: String, required: true, unique: true },
-    stock: { type: Number, required: true },
-    status: { type: Boolean, default: true },
-    category: { type: String, required: true }
-  },
-  {
-    timestamps: true, // agrega createdAt y updatedAt automáticamente
-    versionKey: false // elimina el campo __v que Mongoose agrega por defecto
-  }
-);
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  thumbnails: { type: [String], default: [] },
+  code: { type: String, required: true, unique: true },
+  stock: { type: Number, required: true },
+  status: { type: Boolean, default: true },
+  category: { type: String, required: true }
+});
+
+productSchema.plugin(mongoosePaginate);
 
 const Product = mongoose.model('Product', productSchema);
-
 module.exports = Product;
